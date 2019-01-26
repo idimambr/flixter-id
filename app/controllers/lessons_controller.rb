468d8be86d212adc40235594f6620f8566c118bb
Isabella DiMambro <belladimambro@gmail.com>
2019-01-26 01:02:21 +0000

    weird error message

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_enrolled?, only: [:show]
+
   def show
   end
 
   private
 
+  def user_enrolled?
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled to view the lesson"
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
 end
